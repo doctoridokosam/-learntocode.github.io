2d3085f4ec4c9db3e20dffdd79256724fbbb87ac
Samuel Idoko <doctoridokosam@gmail.com>
2020-06-20 16:50:21 -0700

    my first commit
    my first responsive wed site built from scratch to finish. code life.

---
+++ js/bonsi.js
@@ -0,0 +1,29 @@
+// $(selector).action()
+// $(selector).action(function () {})
+
+
+// $("document").ready(function(){
+     // $("button").click(function(){
+		 // $("P").hide(function(){
+			// $("button").click(function(){
+		// $("p").show();
+	// }); 
+		 // });
+	 // });]
+	
+
+
+ // });
+// $("document").ready(function(){
+	// $('.owl-carousel').owlCarousel();
+	
+	
+			
+ // });
+
+
+// $("input").focus(function(){
+     // $(this).css("background-color", "#cccccc");
+ // }); 
+ 
+	
